78cf95942459cd81caff9ff47032eb4212b61d6b
haruta-otaki <haotaki@davidson.edu>
2026-07-14 16:35:03 -0400

    create dynamicEDT services query and build EDT and create planner node

--- catkin_ws/src/mapping/src/mapping_node.cpp
+++ catkin_ws/src/mapping/src/mapping_node.cpp
@@ -10,8 +10,13 @@
 #include <dynamicEDT3D/dynamicEDTOctomap.h>
 #include <iostream>
 
+// header file generated from the srv file 
+#include <mapping/ScanClearance.h>
+
 octomap::AbstractOcTree* abstract;
 octomap::OcTree* tree; 
+// global pointer 
+DynamicEDTOctomap* distmap = nullptr;
 
 // publish ground_truth/pose as a TF transform 
 void ground_truth_pose_cb(const nav_msgs::Odometry::ConstPtr &msg){
@@ -36,17 +41,11 @@ void octomap_cb(const octomap_msgs::Octomap::ConstPtr &msg){
     tree = dynamic_cast<octomap::OcTree*>(abstract);
 }
 
-int main(int argc, char** argv) {
-    ros::init(argc, argv, "mapping_node");
 
-    ros::NodeHandle nh;
-
-    ros::Subscriber ground_truth_pose_sub = nh.subscribe<nav_msgs::Odometry>
-        ("ground_truth/pose", 10, ground_truth_pose_cb);
-
-    ros::Subscriber octomap_sub = nh.subscribe<octomap_msgs::Octomap>
-        ("octomap_binary", 10, octomap_cb);
-
+// takes in the request and response type defined in the srv file and returns 
+bool build_edt(mapping::ScanClearance::Request  &req,
+        mapping::ScanClearance::Response &res)
+{
     // get minimum and maximum coordinates from octree and create OctoMap point objects
     double x,y,z;
     tree->getMetricMin(x,y,z);
@@ -58,29 +57,63 @@ int main(int argc, char** argv) {
 
     // treat unknown voxels as free (aggressive approach)
     bool unknownAsOccupied = false;
-    float maxDist = 1.0;
-
+    // indoor value
+    float maxDist = 2.0;    
+    if (distmap) {
+        delete distmap;
+    }
     // compute an Euclidean Distance Transform (EDT) over an OctoMap to get maximize clearance from obstacles
     // clamp distance computations and restrict the distance map to a subarea through the arguments
-    DynamicEDTOctomap distmap(maxDist, tree, min, max, unknownAsOccupied);
-
+    distmap = new DynamicEDTOctomap(maxDist, tree, min, max, unknownAsOccupied);    
     // EDT algorithm: walks through the entire octree and computes, for every free voxel, its distance to nearest occupied voxel
     // repeated calls incrementally updates the distances to reflect the modified occupancy map
-    distmap.update(); 
+    distmap->update(); 
+    return true;
+}
 
-    //query 
-    //query point
-    octomap::point3d p(5.0,5.0,0.6);
-    p.x() = min.x() + 0.3 * (max.x() - min.x());
-    p.y() = min.y() + 0.6 * (max.y() - min.y());
-    p.z() = min.z() + 0.5 * (max.z() - min.z());
+bool query_edt(mapping::ScanClearance::Request  &req,
+        mapping::ScanClearance::Response &res)
+{
+    if (!distmap) {
+        ROS_ERROR("distmap not built");
+        return false;
+    }
+
+    distmap->update(); 
+    octomap::point3d p(req.pose.x, req.pose.y, req.pose.z);
 
     octomap::point3d closestObst;
     float distance;
+    geometry_msgs::Point closest_obst_pose; 
     // return the distance of the obstacle from the query point and its location
-    distmap.getDistanceAndClosestObstacle(p, distance, closestObst);
+    distmap->getDistanceAndClosestObstacle(p, distance, closestObst);
+    closest_obst_pose.x = closestObst.x();
+    closest_obst_pose.y = closestObst.y();
+    closest_obst_pose.z = closestObst.z();
     
+    res.clearance = distance;
+    res.closest_obstacle = closest_obst_pose;
+    return true;
+}
+
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "mapping_node");
+
+    ros::NodeHandle nh;
+
+    ros::Subscriber ground_truth_pose_sub = nh.subscribe<nav_msgs::Odometry>
+        ("ground_truth/pose", 10, ground_truth_pose_cb);
+
+    ros::Subscriber octomap_sub = nh.subscribe<octomap_msgs::Octomap>
+        ("octomap_binary", 10, octomap_cb);
+
     // check resolution looks right and confirm coordinate frames are aligned: $ octovis simple_tree.bt
     // tree.writeBinary("tree.bt");
-    delete tree;
+    
+    // service is created and advertised over ROS 
+    ros::ServiceServer build_service = nh.advertiseService("build_edt", build_edt);
+    ros::ServiceServer query_service = nh.advertiseService("query_edt", query_edt);
+
+    ros::spin();
 }
