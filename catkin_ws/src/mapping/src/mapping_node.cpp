40794a03736b3cb3d6617b5a0f1587c48c396a78
haruta-otaki <haotaki@davidson.edu>
2026-07-17 14:36:37 -0400

    debugged logic and cleaned code structure in A* ndoe

--- catkin_ws/src/mapping/src/mapping_node.cpp
+++ catkin_ws/src/mapping/src/mapping_node.cpp
@@ -34,67 +34,6 @@ void ground_truth_pose_cb(const nav_msgs::Odometry::ConstPtr &msg){
     br.sendTransform(transformStamped);
 }
 
-void octomap_cb(const octomap_msgs::Octomap::ConstPtr &msg){
-    // msgToMap() returns AbstractOcTree* as the message could theoretically contain any tree type. 
-    abstract = octomap_msgs::msgToMap(*msg);
-    // get concrete OcTree type
-    tree = dynamic_cast<octomap::OcTree*>(abstract);
-}
-
-// takes in the request and response type defined in the srv file and returns 
-bool build_edt(mapping::ScanClearance::Request  &req,
-        mapping::ScanClearance::Response &res)
-{
-    // get minimum and maximum coordinates from octree and create OctoMap point objects
-    double x,y,z;
-    tree->getMetricMin(x,y,z);
-    octomap::point3d min(x,y,z);
-    //std::cout<<"Metric min: "<<x<<","<<y<<","<<z<<std::endl;
-    tree->getMetricMax(x,y,z);
-    octomap::point3d max(x,y,z);
-    //std::cout<<"Metric max: "<<x<<","<<y<<","<<z<<std::endl;
-
-    // treat unknown voxels as free (aggressive approach)
-    bool unknownAsOccupied = false;
-    // indoor value
-    float maxDist = 2.0;    
-    if (distmap) {
-        delete distmap;
-    }
-    // compute an Euclidean Distance Transform (EDT) over an OctoMap to get maximize clearance from obstacles
-    // clamp distance computations and restrict the distance map to a subarea through the arguments
-    distmap = new DynamicEDTOctomap(maxDist, tree, min, max, unknownAsOccupied);    
-    // EDT algorithm: walks through the entire octree and computes, for every free voxel, its distance to nearest occupied voxel
-    // repeated calls incrementally updates the distances to reflect the modified occupancy map
-    distmap->update(); 
-    return true;
-}
-
-bool query_edt(mapping::ScanClearance::Request  &req,
-        mapping::ScanClearance::Response &res)
-{
-    if (!distmap) {
-        ROS_WARN("edt not built, call build_edt service");
-        return false;
-    }
-
-    distmap->update(); 
-    octomap::point3d p(req.pose.x, req.pose.y, req.pose.z);
-
-    octomap::point3d closestObst;
-    float distance;
-    geometry_msgs::Point closest_obst_pose; 
-    // return the distance of the obstacle from the query point and its location
-    distmap->getDistanceAndClosestObstacle(p, distance, closestObst);
-    closest_obst_pose.x = closestObst.x();
-    closest_obst_pose.y = closestObst.y();
-    closest_obst_pose.z = closestObst.z();
-    
-    res.clearance = distance;
-    res.closest_obstacle = closest_obst_pose;
-    return true;
-}
-
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "mapping_node");
@@ -104,15 +43,8 @@ int main(int argc, char** argv) {
     ros::Subscriber ground_truth_pose_sub = nh.subscribe<nav_msgs::Odometry>
         ("ground_truth/pose", 10, ground_truth_pose_cb);
 
-    ros::Subscriber octomap_sub = nh.subscribe<octomap_msgs::Octomap>
-        ("octomap_binary", 10, octomap_cb);
-
     // check resolution looks right and confirm coordinate frames are aligned: $ octovis simple_tree.bt
     // tree.writeBinary("tree.bt");
-    
-    // service is created and advertised over ROS 
-    ros::ServiceServer build_service = nh.advertiseService("build_edt", build_edt);
-    ros::ServiceServer query_service = nh.advertiseService("query_edt", query_edt);
 
     ros::spin();
 }
--- catkin_ws/src/planner/src/a_star.cpp
+++ catkin_ws/src/planner/src/a_star.cpp
@@ -7,8 +7,6 @@
 #include <unordered_map>
 #include <cmath>
 
-#include <mapping/build_edt.h>
-#include <mapping/query_edt.h>
 #include <planner/plan_path.h>
 
 #include <octomap_msgs/Octomap.h>
@@ -17,74 +15,6 @@
 #include <octomap_msgs/conversions.h>
 
 #include <dynamicEDT3D/dynamicEDTOctomap.h>
-
-#include <bits/stdc++.h>
-using namespace std;
-
-// void octomap_cb(const octomap_msgs::Octomap::ConstPtr &msg){
-// }
-
-octomap::AbstractOcTree* abstract;
-octomap::OcTree* tree; 
-// global pointer 
-DynamicEDTOctomap* distmap = nullptr;
-double resolution = 0.4; 
-double minimum_clearance = 0.45; // iris radius (0.25) + half_voxel (0.2)
-
-
-void octomap_cb(const octomap_msgs::Octomap::ConstPtr &msg){
-    // msgToMap() returns AbstractOcTree* as the message could theoretically contain any tree type. 
-    abstract = octomap_msgs::msgToMap(*msg);
-    // get concrete OcTree type
-    tree = dynamic_cast<octomap::OcTree*>(abstract);
-}
-
-// takes in the request and response type defined in the srv file and returns 
-void build_edt()
-{
-    // get minimum and maximum coordinates from octree and create OctoMap point objects
-    double x,y,z;
-    tree->getMetricMin(x,y,z);
-    octomap::point3d min(x,y,z);
-    //std::cout<<"Metric min: "<<x<<","<<y<<","<<z<<std::endl;
-    tree->getMetricMax(x,y,z);
-    octomap::point3d max(x,y,z);
-    //std::cout<<"Metric max: "<<x<<","<<y<<","<<z<<std::endl;
-
-    // treat unknown voxels as free (aggressive approach)
-    bool unknownAsOccupied = false;
-    // indoor value
-    float maxDist = 2.0;    
-    if (distmap) {
-        delete distmap;
-    }
-    // compute an Euclidean Distance Transform (EDT) over an OctoMap to get maximize clearance from obstacles
-    // clamp distance computations and restrict the distance map to a subarea through the arguments
-    distmap = new DynamicEDTOctomap(maxDist, tree, min, max, unknownAsOccupied);    
-    // EDT algorithm: walks through the entire octree and computes, for every free voxel, its distance to nearest occupied voxel
-    // repeated calls incrementally updates the distances to reflect the modified occupancy map
-    distmap->update(); 
-}
-
-double query_edt(octomap::point3d p)
-{
-    if (!distmap) {
-        ROS_WARN("edt not built, call build_edt service");
-    }
-
-    distmap->update(); 
-
-    octomap::point3d closestObst;
-    float distance;
-    geometry_msgs::Point closest_obst_pose; 
-    // return the distance of the obstacle from the query point and its location
-    distmap->getDistanceAndClosestObstacle(p, distance, closestObst);
-    closest_obst_pose.x = closestObst.x();
-    closest_obst_pose.y = closestObst.y();
-    closest_obst_pose.z = closestObst.z();
-    
-    return distance;
-}
 
 // cell coordinate in sparse hash grid
 struct GridKey {
@@ -157,8 +87,71 @@ public:
     }
 };
 
+// global pointer
 SparseHashGrid hash_grid; 
+octomap::AbstractOcTree* abstract;
+octomap::OcTree* tree;  
+DynamicEDTOctomap* distmap = nullptr;
+// create an alias for pair (tuple)
+typedef std::pair<double, GridKey> Pair;
+double resolution = 0.4; 
+double minimum_clearance = 0.45; // iris radius (0.25) + half_voxel (0.2)
 
+
+void octomap_cb(const octomap_msgs::Octomap::ConstPtr &msg){
+    // msgToMap() returns AbstractOcTree* as the message could theoretically contain any tree type. 
+    abstract = octomap_msgs::msgToMap(*msg);
+    // get concrete OcTree type
+    tree = dynamic_cast<octomap::OcTree*>(abstract);
+    if (distmap) {
+        distmap->update(); 
+    }
+}
+
+// takes in the request and response type defined in the srv file and returns 
+void build_edt()
+{
+    // get minimum and maximum coordinates from octree and create OctoMap point objects
+    double x,y,z;
+    tree->getMetricMin(x,y,z);
+    octomap::point3d min(x,y,z);
+    //std::cout<<"Metric min: "<<x<<","<<y<<","<<z<<std::endl;
+    tree->getMetricMax(x,y,z);
+    octomap::point3d max(x,y,z);
+    //std::cout<<"Metric max: "<<x<<","<<y<<","<<z<<std::endl;
+
+    // treat unknown voxels as free (aggressive approach)
+    bool unknownAsOccupied = false;
+    // indoor value
+    float maxDist = 2.0;    
+    if (distmap) {
+        delete distmap;
+    }
+    // compute an Euclidean Distance Transform (EDT) over an OctoMap to get maximize clearance from obstacles
+    // clamp distance computations and restrict the distance map to a subarea through the arguments
+    distmap = new DynamicEDTOctomap(maxDist, tree, min, max, unknownAsOccupied);    
+    // EDT algorithm: walks through the entire octree and computes, for every free voxel, its distance to nearest occupied voxel
+    // repeated calls incrementally updates the distances to reflect the modified occupancy map
+    distmap->update(); 
+}
+
+double query_edt(octomap::point3d p)
+{
+    if (!distmap) {
+        ROS_WARN("edt not built, call build_edt service");
+    }
+
+    octomap::point3d closestObst;
+    float distance;
+    geometry_msgs::Point closest_obst_pose; 
+    // return the distance of the obstacle from the query point and its location
+    distmap->getDistanceAndClosestObstacle(p, distance, closestObst);
+    closest_obst_pose.x = closestObst.x();
+    closest_obst_pose.y = closestObst.y();
+    closest_obst_pose.z = closestObst.z();
+    
+    return distance;
+}
 
 bool plan_path(planner::plan_path::Request  &req,
         planner::plan_path::Response &res)
@@ -173,45 +166,8 @@ bool plan_path(planner::plan_path::Request  &req,
     Node source = hash_grid.get_or_insert(start_key, start);
     Node destination = hash_grid.get_or_insert(goal_key, goal);
 
-    a_star(source, destination);
+    a_star(start_key, goal_key);
     return true;
-}
-
-
-int main(int argc, char** argv) {
-    ros::init(argc, argv, "planner_node");
-
-    ros::NodeHandle nh;
-
-    // ros::Subscriber ground_truth_pose_sub = nh.subscribe<nav_msgs::Odometry>
-    //     ("ground_truth/pose", 10, ground_truth_pose_cb);
-
-    // check resolution looks right and confirm coordinate frames are aligned: $ octovis simple_tree.bt
-    // tree.writeBinary("tree.bt");
-    
-    // service is created and advertised over ROS 
-    ros::ServiceServer plan_service = nh.advertiseService("plan_path", plan_path);
-
-    ros::Subscriber octomap_sub = nh.subscribe<octomap_msgs::Octomap>
-        ("octomap_binary", 10, octomap_cb);
-
-    return (0);
-}
-
-bool is_valid(octomap::point3d current, octomap::point3d source, octomap::point3d destination)
-{
-    double min_x = std::min(source.x(), destination.x());
-    double max_x = std::max(source.x(), destination.x());
-
-    double min_y = std::min(source.y(), destination.y());
-    double max_y = std::max(source.y(), destination.y());
-
-    double min_z = std::min(source.z(), destination.z());
-    double max_z = std::max(source.z(), destination.z());
-
-    return current.x() >= min_x && current.x() <= max_x &&
-           current.y() >= min_y && current.y() <= max_y &&
-           current.z() >= min_z && current.z() <= max_z;
 }
 
 bool is_obstacle(double clearance, double threshold)
@@ -254,29 +210,27 @@ std::vector<octomap::point3d> generate_path(const GridKey& destination_key)
     std::reverse(path.begin(), path.end());
     return path;
 }
-// create an alias for pair (tuple)
-typedef pair<double, Node> Pair;
 
 // A* Search Algorithm
-void a_star(Node source, Node destination)
+void a_star(GridKey source_key, GridKey destination_key)
 {
     // source or destination is an obstacle (collision gurantee)
-    if (is_obstacle(source.clearance, minimum_clearance) || is_obstacle(destination.clearance, minimum_clearance)) {
+    if (is_obstacle(hash_grid.grid[source_key].clearance, minimum_clearance) || is_obstacle(hash_grid.grid[destination_key].clearance, minimum_clearance)) {
         std::printf("source or destination is aligned with an obstacle \n");
         return;
     }
 
-    if (is_destination(source.point, destination.point)) {
+    if (is_destination(hash_grid.grid[source_key].point, hash_grid.grid[destination_key].point)) {
         return;
     }
 
     // initialize source 
-    source.g = 0.0;
-    source.h = 0.0;
+    hash_grid.grid[source_key].g = 0.0;
+    hash_grid.grid[source_key].h = 0.0;
 
     //  create open list (set) with <f, <i, j>>
-    set<Pair> open_list;
-    open_list.insert(make_pair(source.get_f(), source));
+    std::set<Pair> open_list;
+    open_list.insert(std::make_pair(hash_grid.grid[source_key].get_f(), source_key));
 
     bool found_destination = false;
 
@@ -286,7 +240,7 @@ void a_star(Node source, Node destination)
         // remove vertex from open list
         open_list.erase(open_list.begin());
 
-        p.second.closed = true;
+        hash_grid.grid[p.second].closed = true;
         
         // generate 26 (3 * 3 * 3 - 1) neighbors / successors of p 
 
@@ -302,15 +256,16 @@ void a_star(Node source, Node destination)
                         double dk = k * resolution; 
 
                         // is valid check 
-                        GridKey key {p.second.point.x() + i, p.second.point.y() + j, p.second.point.z() + k};
-                        octomap::point3d point(p.second.point.x() + di, p.second.point.y() + dj, p.second.point.z() + dk); 
+                        GridKey neighbor_key {p.second.x + i, p.second.y + j, p.second.z + k};
+                        octomap::point3d neighbor_point(hash_grid.grid[p.second].point.x() + di,
+                         hash_grid.grid[p.second].point.y() + dj, hash_grid.grid[p.second].point.z() + dk); 
 
-                        Node &neighbor = hash_grid.get_or_insert(key, point); 
+                        Node &neighbor = hash_grid.get_or_insert(neighbor_key, neighbor_point); 
 
                         // neighbor is destination
-                        if (is_destination(point, p.second.point)) {
+                        if (is_destination(neighbor_point, hash_grid.grid[destination_key].point)) {
                             printf("destination found\n");
-                            generate_path(key);
+                            generate_path(neighbor_key); 
                             found_destination = true;
                             return;
                         }
@@ -320,17 +275,17 @@ void a_star(Node source, Node destination)
                             continue; 
                         }
                         // new g, h, f values
-                        double g = neighbor.g + sqrt(di * di + dj * dj + dk * dk); 
-                        double h = calculate_heuristic(point, destination.point); 
+                        double g = hash_grid.grid[p.second].g + sqrt(di * di + dj * dj + dk * dk); 
+                        double h = calculate_heuristic(neighbor_point, hash_grid.grid[destination_key].point); 
                         double f = g + h; 
 
                         // if neighbor is not in open list or has a new minimum f value, insert / update the node
                         if (neighbor.get_f() == std::numeric_limits<double>::max() || neighbor.get_f() > f) {
-                            open_list.insert(make_pair(f, neighbor));
+                            open_list.insert(std::make_pair(f, neighbor_key));
                             neighbor.g = g; 
                             neighbor.h = h; 
                             neighbor.has_parent = true; 
-                            neighbor.parent = hash_grid.world_to_grid(p.second.point);
+                            neighbor.parent = hash_grid.world_to_grid(hash_grid.grid[p.second].point);
                         }
                     }
                 }
@@ -345,3 +300,24 @@ void a_star(Node source, Node destination)
     return;
 }
 
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "planner_node");
+
+    ros::NodeHandle nh;
+
+    // ros::Subscriber ground_truth_pose_sub = nh.subscribe<nav_msgs::Odometry>
+    //     ("ground_truth/pose", 10, ground_truth_pose_cb);
+
+    // check resolution looks right and confirm coordinate frames are aligned: $ octovis simple_tree.bt
+    // tree.writeBinary("tree.bt");
+   
+    ros::Subscriber octomap_sub = nh.subscribe<octomap_msgs::Octomap>
+    ("octomap_binary", 10, octomap_cb);
+
+    // service is created and advertised over ROS 
+    ros::ServiceServer plan_service = nh.advertiseService("plan_path", plan_path);
+
+
+    ros::spin();
+    return (0);
+}
